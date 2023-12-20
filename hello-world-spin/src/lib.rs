32a8ddb1ecf5a85cc97b66683d28e8d80f20f719
N.Shimizu <chikoski@gmail.com>
2023-12-20 11:00:18 +0900

    add tests and routing

--- hello-world-spin/src/lib.rs
+++ hello-world-spin/src/lib.rs
@@ -1,12 +1,24 @@
-use spin_sdk::http::{IntoResponse, Request};
+use spin_sdk::http::{Request, Response, Router};
 use spin_sdk::http_component;
 
 /// A simple Spin HTTP component.
 #[http_component]
-fn handle_hello_world_spin(req: Request) -> anyhow::Result<impl IntoResponse> {
-    println!("Handling request to {:?}", req.header("spin-full-url"));
-    Ok(http::Response::builder()
-        .status(200)
-        .header("content-type", "text/plain")
-        .body("Hello, Fermyon")?)
+fn handle_hello_world_spin(req: Request) -> Response {
+    let mut router = Router::new();
+    router.get("/", api::greet);
+    router.get("/:name", api::greet);
+    router.handle(req)
+}
+
+mod api {
+    use spin_sdk::http::{IntoResponse, Params, Request};
+
+    pub fn greet(_: Request, params: Params) -> anyhow::Result<impl IntoResponse> {
+        let name = params.get("name").unwrap_or("Spin");
+        let greetings = format!("Hello, {}", name);
+        Ok(http::Response::builder()
+            .status(200)
+            .header("content-type", "text/plain")
+            .body(greetings)?)
+    }
 }
