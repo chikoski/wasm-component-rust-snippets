b9a157c24b1844aca8d311948154cb51765a6b04
N.Shimizu <chikoski@gmail.com>
2024-01-28 13:43:49 +0900

    Component composition test comes from wasm component tutorials

---
+++ calculation/add/src/lib.rs
@@ -0,0 +1,11 @@
+cargo_component_bindings::generate!();
+
+use crate::bindings::exports::component::calculation::calculation::Guest;
+
+struct Component;
+
+impl Guest for Component {
+    fn add(a: u32, b: u32) -> u32 {
+        a + b
+    }
+}
---
+++ calculation/cli/src/main.rs
@@ -0,0 +1,9 @@
+cargo_component_bindings::generate!();
+
+use crate::bindings::component::calculation::calculation::add;
+
+fn main() {
+    let a = 1;
+    let b = 2;
+    println!("{a} + {b} = {}", add(a, b));
+}
